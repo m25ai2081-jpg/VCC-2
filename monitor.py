aa4801dec8f2e0b6a6363328b3eba8c05aa03fb5
m25ai2081-jpg <m25ai2081@iitj.ac.in>
2026-03-29 20:12:42 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,24 @@
 import psutil
 import time
 import os
+import subprocess
 
-THRESHOLD = 75.0
-cloud_vm_triggered = False
+HIGH_THRESHOLD = 75
+LOW_THRESHOLD = 30
+COOLDOWN = 60
 
-print("🚀 Starting Local VM Resource Monitoring...")
+last_action_time = 0
+
+def is_vm_running():
+    result = subprocess.run(
+        "gcloud compute instances describe burst-vm-1 --zone=us-central1-a",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    return result.returncode == 0
+
+print("Smart Auto-Scaling Started...\n")
 
 try:
     while True:
@@ -14,21 +27,37 @@ try:
 
         print(f"CPU: {cpu}% | Memory: {memory}%")
 
-        if (cpu > THRESHOLD or memory > THRESHOLD) and not cloud_vm_triggered:
-            print("\n🔥 Threshold exceeded! Triggering GCP VM...\n")
+        vm_running = is_vm_running()  # REAL CHECK
+        current_time = time.time()
 
-            os.system(
-                "gcloud compute instances create burst-vm-1 "
-                "--machine-type=e2-micro "
-                "--zone=us-central1-a "
-                "--tags=http-server "
-                "--metadata-from-file startup-script=start.sh"
-            )
+        # SCALE OUT
+        if (cpu > HIGH_THRESHOLD or memory > HIGH_THRESHOLD) and not vm_running:
+            if current_time - last_action_time > COOLDOWN:
+                print("\nScaling OUT (Creating VM)...\n")
 
-            print("✅ Cloud VM Created Successfully!")
-            cloud_vm_triggered = True
+                os.system(
+                    "gcloud compute instances create burst-vm-1 "
+                    "--machine-type=e2-micro "
+                    "--zone=us-central1-a "
+                    "--tags=http-server "
+                    "--metadata-from-file startup-script=start.sh"
+                )
+
+                last_action_time = current_time
+
+        # SCALE IN
+        elif (cpu < LOW_THRESHOLD and memory < LOW_THRESHOLD) and vm_running:
+            if current_time - last_action_time > COOLDOWN:
+                print("\nScaling IN (Deleting VM)...\n")
+
+                os.system(
+                    "gcloud compute instances delete burst-vm-1 "
+                    "--zone=us-central1-a --quiet"
+                )
+
+                last_action_time = current_time
 
         time.sleep(5)
 
 except KeyboardInterrupt:
-    print("\nStopped by user")
+    print("\nStopped")
